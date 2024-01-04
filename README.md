7a49a8f6aa097d9f23152a97055f5ab34e7f8fc9
Suhail Tech info <114290608+SuhailTechInfo@users.noreply.github.com>
2024-01-04 19:25:01 +0500

    ❤️

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# web-qr
+<h1 align="center"> WEB BASED MULTIDEVICE QR </h1>
+
+  <html>
+   <body>
+  <p align="center">  
+  <a aria-label="FORK" href="https://github.com/SuhailTechInfo/Secktor-bot/fork" target="_blank">
+    <img alt="License: GPL-3" src="https://img.shields.io/github/stars/SuhailTechInfo/web-qr?style=social" target="_blank" />
+  </a>
+  <a aria-label="Suhail_Md is free to use" href="https://youtube.com/@suhailtechinfo" target="_blank">
+    <img alt="SuhailTechInfo" src="https://img.shields.io/youtube/channel/subscribers/UCU071AMRqcd5mfTdCgJFwPg" target="_blank" />
+  </a>
+    <p align="center"><img src="https://profile-counter.glitch.me/{smd-web-qr}/count.svg" alt="SuhailTechInfo :: Visitor's Count" /></p>
+
+     
+  </body>
+</html>
+
+
+## Deployment Methods
+---
+1.  ***Click [`FORK`](https://github.com/SuhailTechInfo/web-qr/fork) and `Star ⭐ Repository` if you get help from repo.***
+2.  ***Deploy on [`HEROKU`](https://dashboard.heroku.com/new?template=https://github.com/SuhailTechInfo/web-qr).***
+3.  ***Deploy on [`REPLIT`](https://replit.com/github/SuhailTechInfo/web-qr).***
+4. ***Deploy on [`RENDER`](https://dashboard.render.com/login).***
+```
+RENDER PROCESS:
+    1: Click "NEW".
+    2: Select "Web Service".
+    3: Click "Build and deploy from a Git repository".
+    4: Now Choose this forked git repo from list.
+    5: And JUST CLICK "Connect". 
+```
+
+
+
+
+- Dependencies
+```sh
+    "@hapi/boom": "^10.0.1",
+    "@whiskeysockets/baileys": "6.5.0",      
+    "pino": "^8.1.0",
+    "express": "^4.18.2",
+    "qrcode": "latest"
+```
+
+
+- WASocket Options
+```js
+    let Smd =SuhailWASocket({ 
+        printQRInTerminal: false,   // make it false if you're using web
+        logger: pino({ level: "silent" }),     // make it silent to prevent baileys buffering
+        browser: Browsers.baileys("Desktop"),  // awailable browsers : ubuntu, macOS, baileys.
+        auth: state 
+    });
+```
+
+
+
+- Getting Session in BASE64
+```js
+    let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
+    var Scan_Id = Buffer.from(CREDS).toString('base64')    // converting into Base64 ---- IMPLEMENT ACCOEDING TO YOUR NEED
+    // res.json({status:true,Scan_Id })
+```
+
+
+
+
+- Environment variable
+```js
+    const PORT = process.env.PORT ||  5000
+    const MESSAGE = process.env.MESSAGE || "Don't Provide your session id to someone!" 
+    //   Set Confirmation Message According to your need! 
+```
+
+
+
+
+
+
+
+
+
+## MADE WITH ❤️
+[![SuhailTechIinfo](https://github.com/SuhailTechInfo.png?size=300)](https://github.com/SuhailTechInfo)
---
+++ index.js
@@ -0,0 +1,143 @@
+
+const express = require("express");
+const app = express();
+
+
+
+
+
+const pino = require("pino");
+let { toBuffer } = require("qrcode");
+const path = require('path');
+const fs = require("fs-extra");
+const { Boom } = require("@hapi/boom");
+const PORT = process.env.PORT ||  5000
+const MESSAGE = process.env.MESSAGE ||  `
+╔════◇
+║ *『 WAOW YOU CHOOSE SUHAIL-MD 』*
+║ _You complete first step to making Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║ *Ytube:* _youtube.com/SuhailTechInfo_
+║ *Owner:* _https://wa.me/923184474176_
+║ *Note :*_Don't provide your SESSION_ID to_
+║ _anyone otherwise that can access chats_
+╚════════════════════════╝
+`
+
+
+
+
+
+
+
+
+
+
+
+if (fs.existsSync('./auth_info_baileys')) {
+    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+  };
+  
+  app.use("/", async(req, res) => {
+
+  const { default: SuhailWASocket, useMultiFileAuthState, Browsers, delay,DisconnectReason, makeInMemoryStore, } = require("@whiskeysockets/baileys");
+  const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+  async function SUHAIL() {
+    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
+    try {
+      let Smd =SuhailWASocket({ 
+        printQRInTerminal: false,
+        logger: pino({ level: "silent" }), 
+        browser: Browsers.baileys("Desktop"),
+        auth: state 
+        });
+
+
+      Smd.ev.on("connection.update", async (s) => {
+        const { connection, lastDisconnect, qr } = s;
+        if (qr) { res.end(await toBuffer(qr)); }
+
+
+        if (connection == "open"){
+          await delay(3000);
+          let user = Smd.user.id;
+
+
+//===========================================================================================
+//===============================  SESSION ID    ===========================================
+//===========================================================================================
+
+          let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
+          var Scan_Id = Buffer.from(CREDS).toString('base64')
+         // res.json({status:true,Scan_Id })
+          console.log(`
+====================  SESSION ID  ==========================                   
+SESSION-ID ==> ${c}
+-------------------   SESSION CLOSED   -----------------------
+`)
+
+
+          let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
+          await Smd.sendMessage(user, { text: MESSAGE } , { quoted : msgsss });
+          await delay(1000);
+          try{ await fs.emptyDirSync(__dirname+'/auth_info_baileys'); }catch(e){}
+
+
+        }
+
+        Smd.ev.on('creds.update', saveCreds)
+
+        if (connection === "close") {            
+            let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+            // console.log("Reason : ",DisconnectReason[reason])
+            if (reason === DisconnectReason.connectionClosed) {
+              console.log("Connection closed!")
+             // SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.connectionLost) {
+                console.log("Connection Lost from Server!")
+            //  SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.restartRequired) {
+                console.log("Restart Required, Restarting...")
+              SUHAIL().catch(err => console.log(err));
+            } else if (reason === DisconnectReason.timedOut) {
+                console.log("Connection TimedOut!")
+             // SUHAIL().catch(err => console.log(err));
+            }  else {
+                console.log('Connection closed with bot. Please run again.');
+                console.log(reason)
+              //process.exit(0)
+            }
+          }
+
+
+
+      });
+    } catch (err) {
+        console.log(err);
+       await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+    }
+  }
+
+
+
+
+
+
+
+
+  SUHAIL().catch(async(err) => {
+    console.log(err)
+    await fs.emptyDirSync(__dirname+'/auth_info_baileys'); 
+
+
+    //// MADE WITH 
+
+});
+
+
+  })
+
+
+app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
